d0afb79a4b15ef9248cb439d240041240e43a00e
Christophe Vanfleteren <c@vanfleteren.net>
2014-10-13 14:29:15 +0200

    Adds tests for idempotent wkt/wkb (de)serialisation.

---
+++ src/test/java/org/geolatte/geom/codec/TestWkbDecodingIdempotent.java
@@ -0,0 +1,31 @@
+package org.geolatte.geom.codec;
+
+import org.geolatte.geom.*;
+import org.geolatte.geom.crs.CrsId;
+import org.geolatte.geom.crs.CrsRegistry;
+import org.geolatte.geom.crs.ProjectedCoordinateReferenceSystem;
+import org.junit.Test;
+
+import static org.geolatte.geom.builder.DSL.p;
+import static org.geolatte.geom.builder.DSL.point;
+import static org.junit.Assert.assertEquals;
+
+/**
+ *
+ */
+public class TestWkbDecodingIdempotent {
+
+    ProjectedCoordinateReferenceSystem crs = CrsRegistry.getProjectedCoordinateReferenceSystemForEPSG(31370);
+
+    @Test
+    public void testWkbConversionShouldBeStable() {
+        Geometry original =  point(crs, p(68878.8400879, 169038.177124));
+
+        String originalWkb = "01010000208A7A000084020071EDD0F040DBFCBF6A71A20441";
+
+        assertEquals(originalWkb, Wkb.toWkb(original).toString());
+
+        // geom to wkb to geom blijft stabliel
+        assertEquals(Wkb.fromWkb(Wkb.toWkb(original)),original);
+    }
+}
---
+++ src/test/java/org/geolatte/geom/codec/TestWktDecodingIdempotent.java
@@ -0,0 +1,28 @@
+package org.geolatte.geom.codec;
+
+import org.geolatte.geom.Geometry;
+import org.geolatte.geom.P2D;
+import org.geolatte.geom.crs.CrsRegistry;
+import org.geolatte.geom.crs.ProjectedCoordinateReferenceSystem;
+import org.junit.Test;
+import static org.geolatte.geom.builder.DSL.*;
+import static org.junit.Assert.*;
+
+/**
+ *
+ */
+public class TestWktDecodingIdempotent {
+
+    ProjectedCoordinateReferenceSystem crs = CrsRegistry.getProjectedCoordinateReferenceSystemForEPSG(31370);
+
+    @Test
+    public void testWktConversionShouldBeIdempotent() {
+        Geometry original =  point(crs, p(68878.8400879, 169038.177124));
+
+        String originalWkt = "SRID=31370;POINT(68878.8400879 169038.177124)";
+        assertEquals(originalWkt, Wkt.toWkt(original));
+
+        // geom to wkb to geom zou stabiel moeten blijven
+        assertEquals(Wkt.fromWkt(Wkt.toWkt(original)),original);
+    }
+}
