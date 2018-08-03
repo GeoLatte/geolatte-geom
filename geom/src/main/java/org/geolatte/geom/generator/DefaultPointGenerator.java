e7b6871f523ef852d82c55f7bcd70372980848a4
Karel Maesen <karel@geovise.com>
2018-08-03 15:32:18 +0200

    Provide seed to Generator for repeatable results

--- geom/src/main/java/org/geolatte/geom/generator/DefaultPointGenerator.java
+++ geom/src/main/java/org/geolatte/geom/generator/DefaultPointGenerator.java
@@ -4,6 +4,8 @@ import org.geolatte.geom.Envelope;
 import org.geolatte.geom.Point;
 import org.geolatte.geom.Position;
 
+import java.util.Random;
+
 import static org.geolatte.geom.generator.PositionGenerator.positionWithin;
 
 /**
@@ -12,14 +14,16 @@ import static org.geolatte.geom.generator.PositionGenerator.positionWithin;
 class DefaultPointGenerator<P extends Position> implements GeometryGenerator<P, Point<P>> {
 
     final private Envelope<P> bbox;
+    final private Random rnd;
 
-    public DefaultPointGenerator(Envelope<P> bbox) {
+    public DefaultPointGenerator(Envelope<P> bbox, Random rnd) {
         this.bbox = bbox;
+        this.rnd = rnd;
     }
 
     @Override
     public Point<P> generate() {
-        return new Point(positionWithin(bbox), bbox.getCoordinateReferenceSystem());
+        return new Point(positionWithin(bbox, rnd), bbox.getCoordinateReferenceSystem());
     }
 
 }
--- geom/src/main/java/org/geolatte/geom/generator/PositionGenerator.java
+++ geom/src/main/java/org/geolatte/geom/generator/PositionGenerator.java
@@ -5,19 +5,21 @@ import org.geolatte.geom.Position;
 import org.geolatte.geom.Positions;
 import org.geolatte.geom.crs.CoordinateReferenceSystem;
 
+import java.util.Random;
+
 /**
  * Created by Karel Maesen, Geovise BVBA on 03/08/2018.
  */
 public class PositionGenerator {
 
-    public static <P extends Position>  P positionWithin(Envelope<P> bbox) {
+    public static <P extends Position>  P positionWithin(Envelope<P> bbox, Random rnd) {
         P ll = bbox.lowerLeft();
         P ur = bbox.upperRight();
 
         CoordinateReferenceSystem<P> crs = bbox.getCoordinateReferenceSystem();
         double[] coords = new double[crs.getCoordinateDimension()];
         for (int i = 0; i < coords.length; i++) {
-            coords[i] = ll.getCoordinate(i) + (ur.getCoordinate(i) - ll.getCoordinate(i)) * Math.random();
+            coords[i] = ll.getCoordinate(i) + (ur.getCoordinate(i) - ll.getCoordinate(i)) * rnd.nextDouble();
         }
         return Positions.mkPosition(crs, coords);
     }
