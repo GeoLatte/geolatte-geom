75007ee685fa603356dbac08ffaf047364a83232
Jeroen van der Vegt <2600739+jvdvegt@users.noreply.github.com>
2019-03-12 10:34:35 +0000

    Fix algorithm to work as documented.
    It seems that with this commit: https://github.com/GeoLatte/geolatte-geom/pull/43/commits/7773d906879c28bef462d3991e1bfe0c09d42a8f the logic broke (commit-comment doesn't match javadoc). Due to 'det == 0' in while-condition, the loop was only executed once.

--- geom/src/main/java/org/geolatte/geom/cga/NumericalMethods.java
+++ geom/src/main/java/org/geolatte/geom/cga/NumericalMethods.java
@@ -80,8 +80,7 @@ public class NumericalMethods {
     /**
      * Determines whether the specified {@code PositionSequence} is counter-clockwise.
      * <p/>
-     * <p>Only the first three positions, are inspected to determine whether the sequence is counter-clockwise.
-     * In case are less than three positions in the sequence, the method returns true.</p>
+     * <p>In case there are less than three positions in the sequence, the method returns true.</p>
      *
      * @param positions a {@code PositionSequence}
      * @return true if the positions in the specified sequence are counter-clockwise, or if the sequence contains
@@ -91,18 +90,18 @@ public class NumericalMethods {
         if (positions.size() < 3) return true;
         Position p0 = positions.getPositionN(0);
         Position p1 = positions.getPositionN(1);
-        double det = 0;
+        double summedDet = 0;
         int positionsSize = positions.size();
         int i = 2;
-        while(i < positionsSize && det == 0) {
+        while(i < positionsSize) {
             Position p2 = positions.getPositionN(i);
-            det = deltaDeterminant(p0, p1, p2);
+            summedDet += deltaDeterminant(p0, p1, p2);
             i++;
         }
-        if (det == 0) {
+        if (summedDet == 0) {
             throw new IllegalArgumentException("Positions are collinear in 2D");
         }
-        return det > 0;
+        return summedDet > 0;
     }
 
     /**
