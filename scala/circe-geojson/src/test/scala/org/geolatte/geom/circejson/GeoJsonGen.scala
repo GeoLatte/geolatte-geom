ae469c7256612e3989070a2de742dee6ea723d1c
Alexander Van Hecke <alexander@avhconsult.be>
2020-06-09 09:00:39 +0200

    add generators for C2D and C3D

--- scala/circe-geojson/src/test/scala/org/geolatte/geom/circejson/GeoJsonGen.scala
+++ scala/circe-geojson/src/test/scala/org/geolatte/geom/circejson/GeoJsonGen.scala
@@ -3,7 +3,7 @@ package org.geolatte.geom.circejson
 import com.fasterxml.jackson.databind.ObjectMapper
 import org.geolatte.geom._
 import org.geolatte.geom.crs.CoordinateReferenceSystems.WGS84
-import org.geolatte.geom.crs.{CoordinateReferenceSystem, CoordinateReferenceSystems, LinearUnit}
+import org.geolatte.geom.crs.{CoordinateReferenceSystem, CoordinateReferenceSystems, CrsRegistry}
 import org.geolatte.geom.generator.GeometryGenerators
 import org.geolatte.geom.json.{GeolatteGeomModule, Setting}
 import org.geolatte.geom.syntax.{GeometryImplicits, PositionBuilder}
@@ -13,21 +13,26 @@ object GeoJsonGen {
 
   import org.geolatte.geom.syntax.GeometryImplicits._
 
-  def buildMapper(crs: CoordinateReferenceSystem[_], settings: (Setting, Boolean)*) : ObjectMapper = {
-    val om = new ObjectMapper()
-    val module =new GeolatteGeomModule(crs)
-    settings.foreach{ case (s, v) => module.set(s,v)}
+  def buildMapper(
+      crs: CoordinateReferenceSystem[_],
+      settings: (Setting, Boolean)*
+  ): ObjectMapper = {
+    val om     = new ObjectMapper()
+    val module = new GeolatteGeomModule(crs)
+    settings.foreach { case (s, v) => module.set(s, v) }
     om.registerModule(module)
     om
   }
 
   private val mapper = buildMapper(WGS84)
 
-  private val noCrsMapper = buildMapper(WGS84, Setting.SUPPRESS_CRS_SERIALIZATION->true)
+  private val noCrsMapper = buildMapper(WGS84, Setting.SUPPRESS_CRS_SERIALIZATION -> true)
 
   implicit class JsonStringable[P <: Position](geom: Geometry[P]) {
-    def asFasterXMLJsonString(withCrs: Boolean = true): String = if (withCrs) mapper.writeValueAsString(geom) else
-      noCrsMapper.writeValueAsString(geom)
+    def asFasterXMLJsonString(withCrs: Boolean = true): String =
+      if (withCrs) mapper.writeValueAsString(geom)
+      else
+        noCrsMapper.writeValueAsString(geom)
   }
 
   /**
@@ -38,7 +43,7 @@ object GeoJsonGen {
       val pb: PositionBuilder[T, P]
   ) {
 
-    private val bbox   = box(crs)(lowerLeft, upperRight)
+    private val bbox  = box(crs)(lowerLeft, upperRight)
     val pointGen      = GeometryGenerators.point(bbox)
     val lineGen       = GeometryGenerators.lineString(5, bbox)
     val polyGen       = GeometryGenerators.polygon(6, bbox)
@@ -56,11 +61,21 @@ object GeoJsonGen {
     )
   }
 
+  val Lambert72C2D: CoordinateReferenceSystem[C2D] =
+    CrsRegistry.getProjectedCoordinateReferenceSystemForEPSG(31370)
+  val Lambert72C3D: CoordinateReferenceSystem[C3D] = Lambert72C2D.addVertical()
+
   implicit val crs2DGen: Gen[CoordinateReferenceSystem[G2D]] =
     Gen.const(CoordinateReferenceSystems.WGS84)
 
+  implicit val crsC2DGen: Gen[CoordinateReferenceSystem[C2D]] =
+    Gen.const(Lambert72C2D)
+
   implicit val crs3DGen: Gen[CoordinateReferenceSystem[G3D]] =
-    Gen.const(CoordinateReferenceSystems.WGS84.addVertical() )
+    Gen.const(CoordinateReferenceSystems.WGS84.addVertical())
+
+  implicit val crsC3DGen: Gen[CoordinateReferenceSystem[C3D]] =
+    Gen.const(Lambert72C3D)
 
   implicit def generator2DSetGen[P <: Position](
       implicit crs: CoordinateReferenceSystem[P],
@@ -92,9 +107,21 @@ object GeoJsonGen {
     point        <- Gen.lzy(generatorSet.pointGen.generate())
   } yield point
 
+  implicit val pointC2DGen: Gen[Point[C2D]] = for {
+    crs          <- crsC2DGen
+    generatorSet <- generator2DSetGen(crs, GeometryImplicits.tupleToC2D)
+    point        <- Gen.lzy(generatorSet.pointGen.generate())
+  } yield point
+
   implicit val point3DGen: Gen[Point[G3D]] = for {
     crs          <- crs3DGen
     generatorSet <- generator3DSetGen(crs, GeometryImplicits.tupleToG3D)
+    point        <- Gen.lzy(generatorSet.pointGen.generate())
+  } yield point
+
+  implicit val pointC3DGen: Gen[Point[C3D]] = for {
+    crs          <- crsC3DGen
+    generatorSet <- generator3DSetGen(crs, GeometryImplicits.tupleToC3D)
     point        <- Gen.lzy(generatorSet.pointGen.generate())
   } yield point
 
@@ -104,9 +131,21 @@ object GeoJsonGen {
     line         <- Gen.lzy(generatorSet.lineGen.generate())
   } yield line
 
+  implicit val lineC2DGen: Gen[LineString[C2D]] = for {
+    crs          <- crsC2DGen
+    generatorSet <- generator2DSetGen(crs, GeometryImplicits.tupleToC2D)
+    line         <- Gen.lzy(generatorSet.lineGen.generate())
+  } yield line
+
   implicit val line3DGen: Gen[LineString[G3D]] = for {
     crs          <- crs3DGen
     generatorSet <- generator3DSetGen(crs, GeometryImplicits.tupleToG3D)
+    line         <- Gen.lzy(generatorSet.lineGen.generate())
+  } yield line
+
+  implicit val lineC3DGen: Gen[LineString[C3D]] = for {
+    crs          <- crsC3DGen
+    generatorSet <- generator3DSetGen(crs, GeometryImplicits.tupleToC3D)
     line         <- Gen.lzy(generatorSet.lineGen.generate())
   } yield line
 
@@ -116,9 +155,21 @@ object GeoJsonGen {
     polygon      <- Gen.lzy(generatorSet.polyGen.generate())
   } yield polygon
 
+  implicit val polygonC2DGen: Gen[Polygon[C2D]] = for {
+    crs          <- crsC2DGen
+    generatorSet <- generator2DSetGen(crs, GeometryImplicits.tupleToC2D)
+    polygon      <- Gen.lzy(generatorSet.polyGen.generate())
+  } yield polygon
+
   implicit val polygon3DGen: Gen[Polygon[G3D]] = for {
     crs          <- crs3DGen
     generatorSet <- generator3DSetGen(crs, GeometryImplicits.tupleToG3D)
+    polygon      <- Gen.lzy(generatorSet.polyGen.generate())
+  } yield polygon
+
+  implicit val polygonC3DGen: Gen[Polygon[C3D]] = for {
+    crs          <- crsC3DGen
+    generatorSet <- generator3DSetGen(crs, GeometryImplicits.tupleToC3D)
     polygon      <- Gen.lzy(generatorSet.polyGen.generate())
   } yield polygon
 
@@ -128,9 +179,21 @@ object GeoJsonGen {
     multiPoint   <- Gen.lzy(generatorSet.multiPointGen.generate())
   } yield multiPoint
 
+  implicit val multiPointC2DGen: Gen[MultiPoint[C2D]] = for {
+    crs          <- crsC2DGen
+    generatorSet <- generator2DSetGen(crs, GeometryImplicits.tupleToC2D)
+    multiPoint   <- Gen.lzy(generatorSet.multiPointGen.generate())
+  } yield multiPoint
+
   implicit val multiPoint3DGen: Gen[MultiPoint[G3D]] = for {
     crs          <- crs3DGen
     generatorSet <- generator3DSetGen(crs, GeometryImplicits.tupleToG3D)
+    multiPoint   <- Gen.lzy(generatorSet.multiPointGen.generate())
+  } yield multiPoint
+
+  implicit val multiPointC3DGen: Gen[MultiPoint[C3D]] = for {
+    crs          <- crsC3DGen
+    generatorSet <- generator3DSetGen(crs, GeometryImplicits.tupleToC3D)
     multiPoint   <- Gen.lzy(generatorSet.multiPointGen.generate())
   } yield multiPoint
 
@@ -140,9 +203,21 @@ object GeoJsonGen {
     multiLine    <- Gen.lzy(generatorSet.multiLineGen.generate())
   } yield multiLine
 
+  implicit val multiLineC2DGen: Gen[MultiLineString[C2D]] = for {
+    crs          <- crsC2DGen
+    generatorSet <- generator2DSetGen(crs, GeometryImplicits.tupleToC2D)
+    multiLine    <- Gen.lzy(generatorSet.multiLineGen.generate())
+  } yield multiLine
+
   implicit val multiLine3DGen: Gen[MultiLineString[G3D]] = for {
     crs          <- crs3DGen
     generatorSet <- generator3DSetGen(crs, GeometryImplicits.tupleToG3D)
+    multiLine    <- Gen.lzy(generatorSet.multiLineGen.generate())
+  } yield multiLine
+
+  implicit val multiLineC3DGen: Gen[MultiLineString[C3D]] = for {
+    crs          <- crsC3DGen
+    generatorSet <- generator3DSetGen(crs, GeometryImplicits.tupleToC3D)
     multiLine    <- Gen.lzy(generatorSet.multiLineGen.generate())
   } yield multiLine
 
@@ -152,9 +227,21 @@ object GeoJsonGen {
     multiPolygon <- Gen.lzy(generatorSet.multiPolyGen.generate())
   } yield multiPolygon
 
+  implicit val multiPolygonC2DGen: Gen[MultiPolygon[C2D]] = for {
+    crs          <- crsC2DGen
+    generatorSet <- generator2DSetGen(crs, GeometryImplicits.tupleToC2D)
+    multiPolygon <- Gen.lzy(generatorSet.multiPolyGen.generate())
+  } yield multiPolygon
+
   implicit val multiPolygon3DGen: Gen[MultiPolygon[G3D]] = for {
     crs          <- crs3DGen
     generatorSet <- generator3DSetGen(crs, GeometryImplicits.tupleToG3D)
+    multiPolygon <- Gen.lzy(generatorSet.multiPolyGen.generate())
+  } yield multiPolygon
+
+  implicit val multiPolygonC3DGen: Gen[MultiPolygon[C3D]] = for {
+    crs          <- crsC3DGen
+    generatorSet <- generator3DSetGen(crs, GeometryImplicits.tupleToC3D)
     multiPolygon <- Gen.lzy(generatorSet.multiPolyGen.generate())
   } yield multiPolygon
 
@@ -167,6 +254,15 @@ object GeoJsonGen {
     multiPolygon2DGen
   )
 
+  implicit val geometryC2DGen: Gen[Geometry[C2D]] = Gen.oneOf(
+    pointC2DGen,
+    lineC2DGen,
+    polygonC2DGen,
+    multiPointC2DGen,
+    multiLineC2DGen,
+    multiPolygonC2DGen
+  )
+
   implicit val geometry3DGen: Gen[Geometry[G3D]] = Gen.oneOf(
     point3DGen,
     line3DGen,
@@ -176,14 +272,33 @@ object GeoJsonGen {
     multiPolygon3DGen
   )
 
+  implicit val geometryC3DGen: Gen[Geometry[C3D]] = Gen.oneOf(
+    pointC3DGen,
+    lineC3DGen,
+    polygonC3DGen,
+    multiPointC3DGen,
+    multiLineC3DGen,
+    multiPolygonC3DGen
+  )
+
   implicit val geometryCollection2DGen: Gen[GeometryCollection[G2D]] = for {
     aantal     <- Gen.choose(1, 10)
     geometries <- Gen.listOfN(aantal, geometry2DGen)
   } yield new GeometryCollection[G2D](geometries: _*)
 
+  implicit val geometryCollectionC2DGen: Gen[GeometryCollection[C2D]] = for {
+    aantal     <- Gen.choose(1, 10)
+    geometries <- Gen.listOfN(aantal, geometryC2DGen)
+  } yield new GeometryCollection[C2D](geometries: _*)
+
   implicit val geometryCollection3DGen: Gen[GeometryCollection[G3D]] = for {
     aantal     <- Gen.choose(1, 10)
     geometries <- Gen.listOfN(aantal, geometry3DGen)
   } yield new GeometryCollection[G3D](geometries: _*)
+
+  implicit val geometryCollectionC3DGen: Gen[GeometryCollection[C3D]] = for {
+    aantal     <- Gen.choose(1, 10)
+    geometries <- Gen.listOfN(aantal, geometryC3DGen)
+  } yield new GeometryCollection[C3D](geometries: _*)
 
 }
--- scala/circe-geojson/src/test/scala/org/geolatte/geom/circejson/GeometryDecodersSpec.scala
+++ scala/circe-geojson/src/test/scala/org/geolatte/geom/circejson/GeometryDecodersSpec.scala
@@ -1,9 +1,9 @@
 package org.geolatte.geom.circejson
 
-import org.geolatte.geom.{G2D, Geometries, Geometry, GeometryType, Position}
 import io.circe.parser.decode
+import org.geolatte.geom.crs.CoordinateReferenceSystems._
 import org.geolatte.geom.crs.{CoordinateReferenceSystems, CrsId}
-import CoordinateReferenceSystems._
+import org.geolatte.geom._
 import org.scalacheck.Gen
 import org.specs2.matcher.MatchResult
 import org.specs2.mutable.Specification
@@ -13,10 +13,9 @@ import org.specs2.mutable.Specification
   */
 class GeometryDecodersSpec extends Specification {
 
+  import GeoJsonGen._
   import org.geolatte.geom.GeometryType._
   import org.geolatte.geom.circe.GeoJsonCodec._
-
-  import GeoJsonGen._
 
   "Decoding CrsID" >> {
     val json =
@@ -31,43 +30,57 @@ class GeometryDecodersSpec extends Specification {
 
     "Decoding a point GeoJson returns a point" >> {
       testScalaCheckGen(point2DGen)
+      testScalaCheckGen(pointC2DGen)
       testScalaCheckGen(point3DGen)
+      testScalaCheckGen(pointC3DGen)
       testEmpty(POINT)
     }
 
     "Decoding a linestring GeoJson returns a linestring" >> {
       testScalaCheckGen(line2DGen)
+      testScalaCheckGen(lineC2DGen)
       testScalaCheckGen(line3DGen)
+      testScalaCheckGen(lineC3DGen)
       testEmpty(LINESTRING)
     }
 
     "Decoding a polygon GeoJson returns a polygon" >> {
       testScalaCheckGen(polygon2DGen)
+      testScalaCheckGen(polygonC2DGen)
       testScalaCheckGen(polygon3DGen)
+      testScalaCheckGen(polygonC3DGen)
       testEmpty(POLYGON)
     }
 
     "Decoding a multipoint GeoJson returns a multipoint" >> {
       testScalaCheckGen(multiPoint2DGen)
+      testScalaCheckGen(multiPointC2DGen)
       testScalaCheckGen(multiPoint3DGen)
+      testScalaCheckGen(multiPointC3DGen)
       testEmpty(MULTIPOINT)
     }
 
     "Decoding a multilinestring GeoJson returns a multilinestring" >> {
       testScalaCheckGen(multiLine2DGen)
+      testScalaCheckGen(multiLineC2DGen)
       testScalaCheckGen(multiLine3DGen)
+      testScalaCheckGen(multiLineC3DGen)
       testEmpty(MULTILINESTRING)
     }
 
     "Decoding a multipolygon GeoJson returns a multipolygon" >> {
       testScalaCheckGen(multiPolygon2DGen)
+      testScalaCheckGen(multiPolygonC2DGen)
       testScalaCheckGen(multiPolygon3DGen)
+      testScalaCheckGen(multiPolygonC3DGen)
       testEmpty(MULTIPOLYGON)
     }
 
     "Decoding a GeometryCollection GeoJson returns a GeometryCollection" >> {
       testScalaCheckGen(geometryCollection2DGen)
+      testScalaCheckGen(geometryCollectionC2DGen)
       testScalaCheckGen(geometryCollection3DGen)
+      testScalaCheckGen(geometryCollectionC3DGen)
       testEmpty(GEOMETRYCOLLECTION)
     }
 
